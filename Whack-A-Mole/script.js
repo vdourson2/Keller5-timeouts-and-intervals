ddb2bb986a0f58cdd18e75d32a8084bbe8a2f4c7
Virginie Dourson <virginiedourson@gmail.com>
2022-12-12 15:34:19 +0100

    Exercice 3 avec un bug restant à corriger

--- Whack-A-Mole/script.js
+++ Whack-A-Mole/script.js
@@ -1,5 +1,18 @@
 let arr = document.querySelectorAll(".cercle");
 let indice = 0;
+let points = 0;
+let score = document.getElementById("score");
+let tpsEcoule = [true, true, true, true, true];
+score.textContent = 0;
+
+//Fonction qui incrément le nombre de points quand on a cliqué sur le cercle rouge
+function increase (indice){
+    if (tpsEcoule[indice] == false){
+        points += 1;
+        console.log("points " + points);
+        score.textContent = points;
+    }
+}
 
 //Fonction qui choisit un cercle aléatoirement, le rend rouge, 
 //et ensuite blanc au bout de 0,95 secondes.
@@ -10,12 +23,20 @@ function dehors () {
         rand = Math.floor(Math.random()*5);
     }
     arr[rand].style.backgroundColor = "red";
-    setTimeout(() => {arr[rand].style.backgroundColor = "white";},950);
+
+    tpsEcoule[rand] = false;
+    arr[rand].addEventListener('click',function() {increase (rand);});
+
+    setTimeout(() => {arr[rand].style.backgroundColor = "white"; tpsEcoule[rand]=true;},1950);
     indice = rand;
    }
 
-const changer = setInterval(dehors,1000);
+const changer = setInterval(dehors,2000);
 
 //Arrêter le setInterval si on clique sur le bouton "arrêter"
 let stop = document.querySelector('button');
 stop.addEventListener('click',() => {clearInterval(changer);})
+
+/*for (let ar of array){
+    ar.addEventListener('')
+}*/
